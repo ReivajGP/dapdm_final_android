148137652a419cdf84986764c2b741800c15c50f
Reivaj Gómez <informaticorp80@gmail.com>
2023-03-07 16:13:05 -0600

    Bottom tab bar is now functional by changing fragments

--- app/src/main/java/com/rgp/feedbapp/activities/MainActivity.kt
+++ app/src/main/java/com/rgp/feedbapp/activities/MainActivity.kt
@@ -2,6 +2,11 @@ package com.rgp.feedbapp.activities
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.navigation.findNavController
+import androidx.navigation.ui.AppBarConfiguration
+import androidx.navigation.ui.setupActionBarWithNavController
+import androidx.navigation.ui.setupWithNavController
+import com.rgp.feedbapp.R
 import com.rgp.feedbapp.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
@@ -10,7 +15,26 @@ class MainActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
+        setupView()
+    }
+
+    // Private methods
+    private fun setupView() {
         binding = ActivityMainBinding.inflate(layoutInflater)
         setContentView(binding.root)
+
+        val navController = findNavController((R.id.navHostFragment))
+
+        val appBarConfig = AppBarConfiguration(
+            setOf(
+                R.id.navigation_home,
+                R.id.navigation_calendar,
+                R.id.navigation_contract,
+                R.id.navigation_profile
+            )
+        )
+
+        setupActionBarWithNavController(navController, appBarConfig)
+        binding.navView.setupWithNavController(navController)
     }
 }
